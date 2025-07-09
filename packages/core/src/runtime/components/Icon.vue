7342aff137b0ce05fcc7a64dd1c815165dc4d512
fan <291950304@qq.com>
2025-07-09 17:56:42 +0800

    feat: Icon component

---
+++ packages/core/src/runtime/components/Icon.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export interface IconProps {
+  name: string
+}
+</script>
+
+<script setup lang="ts">
+import { Icon as IconifyIcon } from '@iconify/vue'
+
+const props = defineProps<IconProps>()
+</script>
+
+<template>
+  <IconifyIcon :icon="name.replace(/^i-/, '')" v-bind="props" />
+</template>
